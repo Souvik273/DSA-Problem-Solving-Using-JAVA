32b8b436eed65ad36a682884d90f589888ba5292
Souvik Goswami <souvikgoswami464@gmail.com>
2024-11-02 12:14:30 +0530

    Time: 0 ms (100%), Space: 45.4 MB (31.91%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,11 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
-        int actualSum = n*(n+1)/2;
-        int arrSum = 0;
-        for(int i =0 ; i<n ; i++){
-            arrSum+=nums[i];
+        int NSum = (n*(n+1))/2;
+        int ArrSum = 0;
+        for(int i=0;i<nums.length;i++){
+            ArrSum += nums[i];
         }
-        return actualSum - arrSum ;
+        return NSum - ArrSum ;
     }
 }
