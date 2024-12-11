8f1823155205b28438626b804578436bbba47235
Souvik Goswami <souvikgoswami464@gmail.com>
2024-12-11 13:14:31 +0530

    Time: 29 ms (83.15%), Space: 60.1 MB (34.71%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,22 +1,22 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        List<Integer> list = new ArrayList<>();
-        Deque<Integer> deque = new LinkedList<>();
-
-        int i=0,j=0;
-        while(j<nums.length){
-            while(!deque.isEmpty() && deque.peekLast()<nums[j])
-                deque.removeLast();
-            deque.addLast(nums[j]);
-            if(j-i+1==k){
-                list.add(deque.peekFirst());
-                if(deque.peekFirst()==nums[i]){
-                    deque.removeFirst();
-                }
-                i++;
+        int n = nums.length;
+        int start = 0 , end = 0;
+        Deque<Integer> possibleMax = new LinkedList<>();
+        int ans[] = new int[n-k+1];
+        int idx = 0;
+        while(end<n){
+            while(possibleMax.size()>0 && possibleMax.peekLast()<nums[end])
+                possibleMax.removeLast();
+            possibleMax.add(nums[end]);
+            if(end-start+1==k){
+                ans[idx++]=possibleMax.peek();
+                if(possibleMax.peek()==nums[start])
+                    possibleMax.removeFirst();
+                start++;
             }
-            j++;
+            end++;
         }
-        return list.stream().mapToInt(Integer::intValue).toArray();
+        return ans;
     }
 }
